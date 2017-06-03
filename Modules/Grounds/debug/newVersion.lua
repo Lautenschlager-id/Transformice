4bfc1a6121c080c87ea0abcf0276eb5b930c3cc9
Lautenschlager <tai.romani@hotmail.com>
2017-06-03 09:07:42 -0300

    Update newVersion.lua

--- Modules/Grounds/debug/newVersion.lua
+++ Modules/Grounds/debug/newVersion.lua
@@ -1,7 +1,7 @@
 ui.new=function(a,b,c,d,e,f,g,h,i)d=d-f/2;e=e-g/2;a=a>0 and a*5 or a;if h then ui.addTextArea(a,"",c,5,5,790,400,1,1,.5,false)end;ui.addTextArea(a+1,"",c,d,e,f,g,0x0C191C,0x0C191C,1,false)ui.addTextArea(a+2,"",c,d+1,e+1,f-2,g-2,0x24474D,0x24474D,1,false)ui.addTextArea(a+3,"",c,d+2,e+2,f-4,g-4,0x183337,0x183337,1,false)ui.addTextArea(a+4,b,c,d+2,e+3,f-4,g-5,i or 0x122528,i or 0x122528,1,false)end
 
 eventNewPlayer = function(n)
-	ui.new(0,"<p align='center'><font size='15'><BV>#GROUNDS MODULE\n\n<p align='justify'><font size='12'>• #Grounds is currently not available due to a new version. Please, wait.\n\n• The module will launch automatically once the update is implemented!\n\n• All the submodes are also disabled.\n\n• Module developed by Bolodefchoco\n\n• Click anywhere to close this window.",nil,400,200,300,220,true)
+	ui.new(0,"<p align='center'><font size='15'><BV>#GROUNDS MODULE\n\n<p align='justify'><font size='12'>• #Grounds is currently not available due to a new version. Please, wait.\n\n• The module will launch automatically once the update is implemented!\n\n• All the submodes are also disabled.\n\n• Module developed by Bolodefchoco\n\n• Click anywhere to close this window.",n,400,200,300,220,true)
 	system.bindMouse(n,true)
 end
 
