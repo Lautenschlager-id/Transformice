48d15fa46025894e72ad9819a0bb4d93e9ccd110
Lautenschlager <tai.romani@hotmail.com>
2018-07-08 21:16:37 -0300

    Update NPC class.lua

--- Others/NPC class.lua
+++ Others/NPC class.lua
@@ -11,30 +11,88 @@ system.looping = function(f, tick)
 	return t
 end
 
+local callback
+do
+	local callback_id = enumIds.callback
+
+	callback = setmetatable({ }, {
+		__call = function(this, eventName, x, y, w, h, d_id)
+			local id = d_id or callback_id
+			local action
+
+			local self = {
+				callback = eventName
+			}
+
+			self.setAction = function(self, f)
+				if type(f) == "function" then
+					action = f
+					this[eventName] = self
+					return true
+				end
+				return false
+			end
+
+			self.performAction = function(self, ...)
+				if action then
+					action(self, ...)
+					return self, true
+				end
+				return self, false
+			end
+
+			self.remove = function(self)
+				ui.removeTextArea(id)
+				this[eventName] = nil
+				return true
+			end
+
+			ui.addTextArea(id, "<textformat leftmargin='1' rightmargin='1'><a href='event:callback." .. eventName .. "'>" .. string.rep('\n', h / 10), nil, x - 5, y - 5, w + 5, h + 5, 1, 1, .1, false)
+
+			if not d_id then
+				callback_id = callback_id + 1
+			end
+
+			return self
+		end
+	})
+end
+
 local npc
 do
 	local npcs = { }
 	
-	local npc_id = 0
+	local npc_id = enumIds.npc
 	
 	npc = function(name, collection, layer)
 		local action
+		local evt
 		local id = npc_id
 		local img
 		local state
 		
-		local deleteImage = function()
+		local self = {
+			x = 0,
+			y = 0,
+			w = 1,
+			h = 1
+		}
+		
+		self.deleteCallback = function(self)
+			if evt then
+				evt:remove()
+				return self
+			end
+			return self, "Callback doesn't exist."
+		end
+	
+		self.destroy = function(self)
 			if img then
 				tfm.exec.removeImage(img)
 				img = nil
 			end
 		end
 		
-		local self = {
-			x = 0,
-			y = 0
-		}
-
 		self.getAction = function(self)
 			if action then
 				return action
@@ -62,6 +120,12 @@ do
 			return state
 		end
 		
+		self.removeAction = function(self)
+			action = nil
+			self.destroy(self)
+			return self
+		end
+		
 		self.setAction = function(self, event, static)
 			if type(state) == "table" then
 				local oldState = state
@@ -75,14 +139,34 @@ do
 					
 					curr = curr + 1
 					
-					deleteImage()
+					self.destroy(self)
 					img = tfm.exec.addImage(state[i] .. ".png", layer, self.x, self.y)
-					ui.addTextArea(id, "<p align='center'><font color='#FFF426' face='Verdana'><B>" .. name, nil, self.x - (static and 20 or 0), self.y - 20, 100, 20, 1, 1, 0, false)
+					ui.addTextArea(id + 1, "<p align='center'><font color='#FFF426' face='Verdana'><B>" .. name, nil, self.x - (static and 20 or 0), self.y - 20, 100, 20, 1, 1, 0, false)
 				end
 
 				return self
 			end
 			return self, "State not set or cannot have an action."
+		end
+		
+		self.setCallback = function(self, eventName, event)
+			if not evt then
+				evt = callback(eventName, self.x, self.y, self.w, self.h, id)
+				
+				if event then
+					evt:setAction(event)
+				end
+			
+				return self
+			end
+			return self, "Callback already exists."
+		end
+		
+		self.setDimension = function(self, w, h)
+			self.w = w
+			self.h = h
+			
+			return self
 		end
 		
 		self.setPosition = function(self, x, y)
@@ -97,7 +181,7 @@ do
 			
 			if newState and state ~= newState then
 				state = newState
-				self.stop(self)
+				self.removeAction(self)
 
 				return self
 			end
@@ -108,7 +192,7 @@ do
 			if state then
 				if #state == 1 then
 					self.setAction(self, function()
-						self.stop(self)
+						self.removeAction(self)
 					end, true)
 					
 					return self
@@ -117,15 +201,9 @@ do
 			end
 			return self, "State not set."
 		end
-		
-		self.stop = function(self)
-			action = nil
-			deleteImage()
-			return self
-		end
-		
+
 		npcs[#npcs + 1] = self
-		npc_id = npc_id + 1
+		npc_id = npc_id + 2
 		return self
 	end
 
@@ -138,4 +216,3 @@ do
 		end
 	end, 10)
 end
-
