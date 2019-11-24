55085509a4e09d36b57ec7117ec39651948b6d95
Lautenschlager <tai.romani@hotmail.com>
2019-11-24 17:35:42 -0300

    Create callback.lua

---
+++ Others/callback.lua
@@ -0,0 +1,128 @@
+local callback
+do
+	local id = 0
+
+	callback = {
+		_instance = {
+			_count = 0
+		}
+	}
+	callback.__index = callback
+	callback.__iter = function()
+		return ipairs(callback._instance)
+	end
+	callback.__get = function(eventName)
+		return callback._instance[callback._instance[eventName]]
+	end
+
+	callback.new = function(eventName, x, y, width, height, hideCallback)
+		id = id + 1
+
+		local self = setmetatable({
+			id = id,
+			eventName = eventName,
+			x = x,
+			y = y,
+			width = width,
+			height = height,
+			isFixed = false,
+			hasRange = false,
+			borderRange = 0,
+			action = nil,
+			isActive = false,
+			_blockedPlayers = { }
+		}, callback)
+
+		if not hideCallback then
+			self:display()
+		end
+
+		local instance = callback._instance
+		instance._count = instance._count + 1
+		instance[instance._count] = self
+		instance[eventName] = instance._count
+
+		return self
+	end
+
+	callback.textarea = function(self, playerName)
+		if playerName then
+			self._blockedPlayers[playerName] = false
+		end
+
+		ui.addTextArea(self.id, "<textformat leftmargin='1' rightmargin='1'><a href='event:callback." .. self.eventName .. "'>" .. string.rep('\n', self.height / 10), playerName, self.x - 5, self.y - 5, self.width + 5, self.height + 5, 1, 1, (DEBUG and .2 or 0), self.isFixed)
+
+		return self
+	end
+
+	callback.display = function(self, f)
+		if type(f) ~= "function" then
+			-- Nil, Nickname, ...
+			self:textarea(f)
+		else
+			for playerName, data in next, playerCache do
+				if f(playerName, data) then
+					self:textarea(playerName)
+				end
+			end
+		end
+		self.isActive = true
+
+		return self
+	end
+
+	callback.setFixed = function(self)
+		self.isFixed = true
+		return self
+	end
+
+	callback.setClickableRange = function(self, addToBorder)
+		if addToBorder then
+			self.borderRange = addToBorder
+		end
+		self.hasRange = true
+
+		return self
+	end
+
+	callback.inClickableRange = function(self, playerName, x, y)
+		if not self.isActive or self._blockedPlayers[playerName] then return end
+		if not self.hasRange then
+			-- Can be triggered in any position
+			return true
+		end
+
+		return (
+			(
+				x >= (self.x - self.borderRange) and
+				x <= (self.x + self.width + self.borderRange)
+			) and
+			(
+				y >= (self.y - self.borderRange) and
+				y <= (self.y + self.height + self.borderRange)
+			)
+		)
+	end
+
+	callback.setAction = function(self, action)
+		self.action = action
+		return self
+	end
+
+	callback.performAction = function(self, playerName, x, y, ...)
+		if not self.action then return end
+		if not self:inClickableRange(playerName, x, y) then return end
+
+		return self:action(playerName, x, y, ...) -- self, playerName, x, y, ...
+	end
+
+	callback.remove = function(self, playerName)
+		if not playerName then
+			self.isActive = false
+		else
+			self._blockedPlayers[playerName] = true
+		end
+
+		ui.removeTextArea(self.id, playerName)
+	end
+end
